23c91f41ac1a7ef289663e47ad94310c4ea8456a
Eugen263 <kupulenko29@gmail.com>
2022-01-27 02:32:54 +0200

    v1.0.2

---
+++ src/components/Head.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import classes from "../styles/head.module.css";
+
+const Head = () => {
+    return (
+        <div className={"midEl"}>
+            <h1 className={classes.logoText}>FreeLancin.io</h1>
+        </div>
+    );
+};
+
+export default Head;
